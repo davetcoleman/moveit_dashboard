40c0d5af66c56dcff8868f370e779955a30b8ac9
Dave Coleman <davetcoleman@gmail.com>
2015-11-10 14:03:03 -0800

    Initial commit

---
+++ src/moveit_panel.cpp
@@ -0,0 +1,260 @@
+/*********************************************************************
+ * Software License Agreement (BSD License)
+ *
+ *  Copyright (c) 2015, PickNik LLC
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of PickNik LLC nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *********************************************************************/
+
+/* Author: Dave Coleman
+   Desc:   Rviz display panel for controlling and debugging MoveIt! applications
+*/
+
+#include <stdio.h>
+
+#include <QPainter>
+#include <QLineEdit>
+#include <QVBoxLayout>
+#include <QHBoxLayout>
+#include <QLabel>
+#include <QTimer>
+#include <QGroupBox>
+#include <QSpinBox>
+
+#include <std_msgs/Bool.h>
+
+#include "moveit_panel.h"
+
+namespace moveit_dashboard
+{
+MoveItPanel::MoveItPanel(QWidget* parent)
+  : rviz::Panel(parent)
+{
+  // Create a push button
+  btn_next_ = new QPushButton(this);
+  btn_next_->setText("Next");
+  connect(btn_next_, SIGNAL(clicked()), this, SLOT(moveNext()));
+
+  // Create a push button
+  btn_auto_ = new QPushButton(this);
+  btn_auto_->setText("Auto");
+  connect(btn_auto_, SIGNAL(clicked()), this, SLOT(moveAuto()));
+
+  // Create a push button
+  btn_full_auto_ = new QPushButton(this);
+  btn_full_auto_->setText("Full Auto");
+  connect(btn_full_auto_, SIGNAL(clicked()), this, SLOT(moveFullAuto()));
+
+  // Create a push button
+  btn_stop_ = new QPushButton(this);
+  btn_stop_->setText("Stop");
+  connect(btn_stop_, SIGNAL(clicked()), this, SLOT(moveStop()));
+
+  // Create a push button
+  btn_reset_ = new QPushButton(this);
+  btn_reset_->setText("Reset");
+  connect(btn_reset_, SIGNAL(clicked()), this, SLOT(resetRobot()));
+
+  // Create a push button
+  btn_bringup_ = new QPushButton(this);
+  btn_bringup_->setText("Bringup");
+  connect(btn_bringup_, SIGNAL(clicked()), this, SLOT(bringupRobot()));
+
+  // Create a push button
+  btn_home_ = new QPushButton(this);
+  btn_home_->setText("Home");
+  connect(btn_home_, SIGNAL(clicked()), this, SLOT(homeRobot()));
+
+  // Create a push button
+  btn_grip_ = new QPushButton(this);
+  btn_grip_->setText("Grip");
+  connect(btn_grip_, SIGNAL(clicked()), this, SLOT(toggleGripper()));
+
+  // Drop down box
+  // combo_mode_ = new QComboBox(this);
+  // combo_mode_->addItem("");
+  // combo_mode_->addItem("Brake");
+  // combo_mode_->addItem("Joint Position");
+  // combo_mode_->addItem("Gravity Compensation");
+  // combo_mode_->addItem("Joint Impedence - Low");
+  // combo_mode_->addItem("Joint Impedence - Medium");
+  // combo_mode_->addItem("Joint Impedence - High");
+  // connect(combo_mode_, SIGNAL(currentIndexChanged(int)), this, SLOT(changeJointMode(int)));
+
+  // Gripping force
+  // spin_box_ = new QSpinBox(this);
+  // spin_box_->setRange(5, 50);
+  // spin_box_->setWrapping(true);
+  // spin_box_->setValue(5);
+  // spin_box_->stepBy(1);
+
+  // Horizontal Layout
+  QHBoxLayout* hlayout1 = new QHBoxLayout;
+  hlayout1->addWidget(btn_next_);
+  hlayout1->addWidget(btn_auto_);
+  hlayout1->addWidget(btn_full_auto_);
+  hlayout1->addWidget(btn_stop_);
+
+  // Horizontal Layout
+  QHBoxLayout* hlayout2 = new QHBoxLayout;
+  //hlayout2->addWidget(new QLabel(QString("Robot:")));
+  hlayout2->addWidget(btn_bringup_);
+  hlayout2->addWidget(btn_home_);
+  hlayout2->addWidget(btn_reset_);
+  hlayout2->addWidget(btn_grip_);
+  //hlayout2->addWidget(spin_box_);
+  //hlayout2->addWidget(combo_mode_);
+
+  // Horizontal Layout
+  // QHBoxLayout* hlayout3 = new QHBoxLayout;
+
+  this->setStyleSheet("QGroupBox {  border: 1px solid gray; padding-top: 5px; }");
+
+  // Group box
+  QGroupBox* group_box = new QGroupBox();
+  group_box->setLayout(hlayout2);
+  group_box->setFlat(false);
+
+  // Verticle layout
+  QVBoxLayout* layout = new QVBoxLayout;
+  layout->addLayout(hlayout1);
+  layout->addWidget(group_box);
+  // layout->addLayout( hlayout3 );
+  setLayout(layout);
+
+  remote_publisher_ =
+    nh_.advertise<dashboard_msgs::DashboardControl>("/moveit_main/remote_control", 1);
+
+  // Make the control widget start disabled, since we don't start with an output topic.
+  btn_next_->setEnabled(true);
+  btn_auto_->setEnabled(true);
+  btn_full_auto_->setEnabled(true);
+}
+
+void MoveItPanel::moveNext()
+{
+  ROS_INFO_STREAM_NAMED("moveit_dashboard", "Move to next step");
+
+  dashboard_msgs::DashboardControl msg;
+  msg.next_step = true;
+  remote_publisher_.publish(msg);
+}
+
+void MoveItPanel::moveAuto()
+{
+  ROS_INFO_STREAM_NAMED("moveit_dashboard", "Running auto step");
+
+  dashboard_msgs::DashboardControl msg;
+  msg.auto_step = true;
+  remote_publisher_.publish(msg);
+}
+
+void MoveItPanel::moveFullAuto()
+{
+  ROS_INFO_STREAM_NAMED("moveit_dashboard", "Running auto trajectory");
+
+  dashboard_msgs::DashboardControl msg;
+  msg.full_auto = true;
+  remote_publisher_.publish(msg);
+}
+
+void MoveItPanel::moveStop()
+{
+  ROS_INFO_STREAM_NAMED("moveit_dashboard", "Stopping");
+
+  dashboard_msgs::DashboardControl msg;
+  msg.stop = true;
+  remote_publisher_.publish(msg);
+}
+
+void MoveItPanel::changeJointMode(int mode)
+{
+  ROS_INFO_STREAM_NAMED("moveit_dashboard", "Changing joint mode");
+
+  dashboard_msgs::DashboardControl msg;
+  msg.change_joint_mode = true;
+  msg.joint_mode = mode;
+  remote_publisher_.publish(msg);
+}
+
+void MoveItPanel::resetRobot()
+{
+  ROS_INFO_STREAM_NAMED("moveit_dashboard", "Resetting robot");
+
+  dashboard_msgs::DashboardControl msg;
+  msg.robot_reset = true;
+  remote_publisher_.publish(msg);
+}
+
+void MoveItPanel::bringupRobot()
+{
+  ROS_INFO_STREAM_NAMED("moveit_dashboard", "Bringing up robot");
+
+  dashboard_msgs::DashboardControl msg;
+  msg.robot_bringup = true;
+  remote_publisher_.publish(msg);
+}
+
+void MoveItPanel::homeRobot()
+{
+  ROS_INFO_STREAM_NAMED("moveit_dashboard", "Sending robot home");
+
+  // Ensure robot is in joint position or impedance mode
+  //combo_mode_->setCurrentIndex(4);  // Low impedance
+
+  dashboard_msgs::DashboardControl msg;
+  msg.robot_home = true;
+  remote_publisher_.publish(msg);
+}
+
+void MoveItPanel::toggleGripper()
+{
+  ROS_INFO_STREAM_NAMED("moveit_dashboard", "Toggling gripping");
+
+  dashboard_msgs::DashboardControl msg;
+  msg.toggle_gripper = true;
+  int force = spin_box_->value();
+  msg.toggle_gripper_force = force;
+  remote_publisher_.publish(msg);
+}
+
+// Save all configuration data from this panel to the given
+// Config object.  It is important here that you call save()
+// on the parent class so the class id and panel name get saved.
+void MoveItPanel::save(rviz::Config config) const { rviz::Panel::save(config); }
+// Load all configuration data for this panel from the given Config object.
+void MoveItPanel::load(const rviz::Config& config) { rviz::Panel::load(config); }
+}  // end namespace moveit_dashboard
+
+// Tell pluginlib about this class.  Every class which should be
+// loadable by pluginlib::ClassLoader must have these two lines
+// compiled in its .cpp file, outside of any namespace scope.
+#include <pluginlib/class_list_macros.h>
+PLUGINLIB_EXPORT_CLASS(moveit_dashboard::MoveItPanel, rviz::Panel)
+// END_TUTORIAL
---
+++ src/moveit_panel.h
@@ -0,0 +1,146 @@
+/*********************************************************************
+ * Software License Agreement (BSD License)
+ *
+ *  Copyright (c) 2015, PickNik LLC
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of PickNik LLC nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *********************************************************************/
+
+/* Author: Dave Coleman
+   Desc:   Rviz display panel for controlling and debugging MoveIt! applications
+*/
+
+// TODO: convert to flow layout:
+// http://doc.qt.io/qt-5/qtwidgets-layouts-flowlayout-example.html
+
+#ifndef MOVEIT_DASHBOARD__MOVEIT_PANEL_H
+#define MOVEIT_DASHBOARD__MOVEIT_PANEL_H
+
+#ifndef Q_MOC_RUN
+#include <ros/ros.h>
+
+#include <rviz/panel.h>
+#endif
+
+#include <QPushButton>
+#include <QComboBox>
+#include <dashboard_msgs/DashboardControl.h>
+
+class QLineEdit;
+class QSpinBox;
+
+namespace moveit_dashboard
+{
+// BEGIN_TUTORIAL
+// Here we declare our new subclass of rviz::Panel.  Every panel which
+// can be added via the Panels/Add_New_Panel menu is a subclass of
+// rviz::Panel.
+//
+// MoveItPanel will show a text-entry field to set the output topic
+// and a 2D control area.  The 2D control area is implemented by the
+// DriveWidget class, and is described there.
+class MoveItPanel : public rviz::Panel
+{
+  // This class uses Qt slots and is a subclass of QObject, so it needs
+  // the Q_OBJECT macro.
+  Q_OBJECT
+public:
+  // QWidget subclass constructors usually take a parent widget
+  // parameter (which usually defaults to 0).  At the same time,
+  // pluginlib::ClassLoader creates instances by calling the default
+  // constructor (with no arguments).  Taking the parameter and giving
+  // a default of 0 lets the default constructor work and also lets
+  // someone using the class for something else to pass in a parent
+  // widget as they normally would with Qt.
+  MoveItPanel(QWidget *parent = 0);
+
+  // Now we declare overrides of rviz::Panel functions for saving and
+  // loading data from the config file.  Here the data is the
+  // topic name.
+  virtual void load(const rviz::Config &config);
+  virtual void save(rviz::Config config) const;
+
+  // Next come a couple of public Qt slots.
+public Q_SLOTS:
+
+  // Here we declare some internal slots.
+protected Q_SLOTS:
+
+  /// User has chosen to go to next step
+  void moveNext();
+
+  /// User has chosen to run demo
+  void moveAuto();
+
+  /// User has chosen to run demo
+  void moveFullAuto();
+
+  /// User has chosen to stop
+  void moveStop();
+
+  /// User wants to change joint mode (velocity/gravity compensation)
+  void changeJointMode(int mode);
+
+  /// User wants to reset robot
+  void resetRobot();
+
+  /// User wants to bringup robot
+  void bringupRobot();
+
+  /// User wants to send robot home
+  void homeRobot();
+
+  /// User wants to bringup robot
+  void toggleGripper();
+
+  // Then we finish up with protected member variables.
+protected:
+  QPushButton *btn_next_;
+  QPushButton *btn_auto_;
+  QPushButton *btn_full_auto_;
+  QPushButton *btn_stop_;
+  QPushButton *btn_reset_;
+  QPushButton *btn_bringup_;
+  QPushButton *btn_home_;
+  QPushButton *btn_grip_;
+  QSpinBox *spin_box_;
+  QComboBox *combo_mode_;
+
+  // The ROS publishers
+  ros::Publisher remote_publisher_;
+
+  // The ROS node handle.
+  ros::NodeHandle nh_;
+
+  // END_TUTORIAL
+};
+
+}  // end namespace moveit_dashboard
+
+#endif  // MOVEIT_DASHBOARD__MOVEIT_PANEL_H
